5f4b57c41d5313b77ea3b390a7485e0ccae6b62b
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2022-03-11 11:57:41 +0100

    Added calculator

---
+++ c++ calculator/C++ opdracht/C++ opdracht.cpp
@@ -0,0 +1,27 @@
+// C++ opdracht.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int Plus(int a, int b) {
+	return a + b;
+}
+int Minus(int a, int b) {
+	return a - b;
+}
+
+int Mulitply(int a, int b) {
+	return a * b;
+}
+
+int Divide(float a, float b) {
+	return a / b;
+}
+
+int Root(int a) {
+	return sqrt(a);
+}
+
+int main() {
+
+}
